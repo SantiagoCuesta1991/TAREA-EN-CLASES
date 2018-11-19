9645e745b56665535960c41dd02f27823b8a133d
MarcoPozo <MarcoPozo@DESKTOP-307OBHN>
2018-11-19 12:30:54 -0500

    tarea en clase

---
+++ src/modelo/Pallets.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ * @Instituto Rumiñahui
+ * @author Santiagocuesta
+ */
+public class Pallets extends Productos{
+    private char [] palletTyoe;
+
+    public Pallets(char rawMaterial, char rawM_Accesories, char productFinished) {
+        super(rawMaterial, rawM_Accesories, productFinished);
+    }
+
+    /**
+     * @return the palletTyoe
+     */
+    public char[] getPalletTyoe() {
+        return palletTyoe;
+    }
+
+    /**
+     * @param palletTyoe the palletTyoe to set
+     */
+    public void setPalletTyoe(char[] palletTyoe) {
+        this.palletTyoe = palletTyoe;
+    }
+
+    
+    
+}
---
+++ src/modelo/SelectOperation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ * @Instituto Rumiñahui
+ * @author Santiagocuesta
+ */
+public interface SelectOperation {
+    //public void selectOperation();
+    public char jobRobot();
+    public char jobCMC();
+    public char josInspection(); 
+}
